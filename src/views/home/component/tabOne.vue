dbe7d66f304a15363264596a3dbfb7ad86df75bb
groot <czgroot1314@gmail.com>
2024-06-28 15:01:30 +0800

    首页

--- src/views/home/component/tabOne.vue
+++ src/views/home/component/tabOne.vue
@@ -1,64 +1,68 @@
 <template>
     <div class="one-page">
+        <!-- 轮播图 -->
         <van-swipe class="my-swipe" :autoplay="3000">
             <van-swipe-item v-for="(image, index) in images" :key="index">
-                <!-- <img  width="100% " height="100" :src="image"/> -->
-                {{image}}
+                <img  width="100% " height="100%" :src="image"/>
             </van-swipe-item>
         </van-swipe>
-        <van-pull-refresh v-model="isLoading" @refresh="onRefresh" style="min-height: calc(100% - 44px);">
+
+        <!-- 主体页面 -->
+        <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
             <div>
                 <div class="video-title">
-                    <span>热门影片</span>
-                    <span>热门影片</span>
+                    <span>
+                        <van-icon name="fire-o"/>热门影片
+                    </span>
+                    <span>
+                        <span>
+                            更多<van-icon name="arrow"/>
+                        </span>
+                        <span class="row-num">
+                            <van-icon name="qr"/>
+                            <van-dropdown-menu>
+                                <van-dropdown-item v-model="rowNum" :options="option1" />
+                            </van-dropdown-menu>
+                        </span>
+                    </span>
                 </div>
-                <van-grid :column-num="2">
-                    <van-grid-item v-for="img in imageList">
-                        <img :src="img" v-lazy="img" />
-                    </van-grid-item>
-                </van-grid>
+                <div class="grid-list">
+                    <van-grid :column-num="1">
+                        <van-grid-item v-for="img in imageList">
+                            <img :src="img" v-lazy="img" />
+                        </van-grid-item>
+                    </van-grid>
+                </div>
             </div>
         </van-pull-refresh>
     </div>
 </template>
 <script>
 import { Toast } from 'vant';
+import imageList from '../data.js';
 export default {
     data() {
         return {
             images: [
-                "'../../../assets/image/lunbo1.jpg'",
-                "'../../../assets/image/lunbo2.jpg'",
-                "'../../../assets/image/lunbo3.jpg'",
+                require('../../../assets/image/lunbo1.jpg'),
+                require('../../../assets/image/lunbo2.jpg'),
+                require('../../../assets/image/lunbo3.jpg'),
             ],
             isLoading: false,
-            imageList: [
-                'https://img01.yzcdn.cn/vant/apple-1.jpg',
-                'https://img01.yzcdn.cn/vant/apple-2.jpg',
-                'https://img01.yzcdn.cn/vant/apple-1.jpg',
-                'https://img01.yzcdn.cn/vant/apple-2.jpg',
-                'https://img01.yzcdn.cn/vant/apple-1.jpg',
-                'https://img01.yzcdn.cn/vant/apple-2.jpg',
-                'https://img01.yzcdn.cn/vant/apple-1.jpg',
-                'https://img01.yzcdn.cn/vant/apple-2.jpg',
-                'https://img01.yzcdn.cn/vant/apple-1.jpg',
-                'https://img01.yzcdn.cn/vant/apple-2.jpg',
-                'https://img01.yzcdn.cn/vant/apple-1.jpg',
-                'https://img01.yzcdn.cn/vant/apple-2.jpg',
-                'https://img01.yzcdn.cn/vant/apple-1.jpg',
-                'https://img01.yzcdn.cn/vant/apple-2.jpg',
-                'https://img01.yzcdn.cn/vant/apple-1.jpg',
-                'https://img01.yzcdn.cn/vant/apple-2.jpg',
-                'https://img01.yzcdn.cn/vant/apple-1.jpg',
-                'https://img01.yzcdn.cn/vant/apple-2.jpg',
-                'https://img01.yzcdn.cn/vant/apple-1.jpg',
-                'https://img01.yzcdn.cn/vant/apple-2.jpg',
-                'https://img01.yzcdn.cn/vant/apple-1.jpg',
-                'https://img01.yzcdn.cn/vant/apple-2.jpg',
-                'https://img01.yzcdn.cn/vant/apple-1.jpg',
-                'https://img01.yzcdn.cn/vant/apple-2.jpg',
+            imageList,
+            rowNum: 1,
+            option1: [
+                { text: '1列', value: 1 },
+                { text: '2列', value: 2 },
+                { text: '3列', value: 3 },
+                { text: '4列', value: 4 },
             ],
         };
+    },
+    created() {
+        
+        console.log(this.imageList)
+        console.log('this.imageList')
     },
     methods: {
         //下拉刷新
@@ -92,10 +96,30 @@ export default {
     }
 }
 
+/*轮播图*/
+.van-swipe-item{
+    height: 120px;
+}
+
 /*视频标题*/
 .video-title{
     display:flex;
     justify-content: space-between; 
-    padding: 15px 15px;
+    padding: 10px 10px;
+    .van-icon-fire-o{
+        color: red;
+        font-weight: 800;
+        padding-right: 6px;
+    }
+    .row-num{
+        padding-left: 8px;
+    }
+}
+
+.van-grid{
+    padding:8px
+}
+.van-grid-item{
+    padding:8px
 }
 </style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,38 +1,77 @@
 <template>
     <div class="indexBox">
-        <div class="header"><app-heder></app-heder></div>
+        <div class="header">
+            <div class="header-page">
+                <div>
+                    LoGo
+                </div>
+                <div>
+                    <van-search v-model="value" show-action shape="round" show-action-color='red' background="#2A2A2A" placeholder="请输入关键词" @click="onSearch(value)">
+                        <template #action>
+                            <van-button round type="info" size="small" icon="search" color="#454545" @click="onSearch(value)">搜索</van-button>
+                        </template>
+                    </van-search>
+                </div>
+                <van-button round type="info" size="small" icon="filter-o" color="#454545" @click="onSearch(value)">筛选</van-button>
+            </div>
+        </div>
         <main class="main">
-            <app-main></app-main>
-            <app-navbar></app-navbar>
+            <div class="main-page">
+                <router-view></router-view>
+            </div>
+            
+            <div class="navbar" route>
+                <van-tabbar class="bottom_menu" v-model="active" active-color="#3899FB" inactive-color="#FFF">
+                    <van-tabbar-item icon="home-o" name="/home" to="/home" >首页</van-tabbar-item>
+                    <van-tabbar-item icon="search" name="/search" to="/search">查询</van-tabbar-item>
+                    <van-tabbar-item icon="friends-o" name="/user" to="/user">用户</van-tabbar-item>
+                    <van-tabbar-item icon="setting-o" name="/setting" to="/setting">设置</van-tabbar-item>
+                </van-tabbar>
+            </div>
         </main>
     </div>
 </template>
 
 <script>
-import AppHeder from "@/components/AppHeader/index.vue"
-import AppMain from "@/components/AppMain/index.vue"
-import AppNavbar from "@/components/AppNavbar/index.vue"
 export default {
-    components: {
-        AppHeder,
-        AppMain,
-        AppNavbar
+    data () {
+        return {
+            value: '',
+            active: 0
+        }
     },
     created() {
-
+        switch(this.$route.path){
+            case '/search':
+                this.active = 1;
+                break;
+            case '/user':
+                this.active = 2;
+                break;
+            case '/setting':
+                this.active = 3;
+                break;
+            default:
+        }
+        console.log(this.$route.path)
     },
     methods: {
-
+        onSearch(val){
+            console.log(val)
+        },
     },
 };
 </script>
-<style>
+
+<style scoped lang="scss">
+/*首页最外层*/
 .indexBox{
     display: flex;
     flex-direction: column;
     position: relative;
     height: 100vh;
 }
+/*头部*/
 .header{
     height: 54px;
     width: 100%;
@@ -40,8 +79,54 @@ export default {
     top: 0;
     left: 0;
 }
+.header-page {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .van-search__action{
+        display: flex;
+        align-items: center;
+    }
+    div{
+        color: #8d8d8d;
+    }
+
+    /*搜索框*/
+    .van-search__content{
+        background-color: #454545;
+    }
+
+    /*搜索框文案*/
+    ::v-deep .van-field__control::placeholder {
+        color: #8d8d8d;
+    }
+
+    /*搜索框icon*/
+    ::v-deep .van-field__left-icon{
+        display: none;
+    }
+}
+
+
+/*主体*/
 .main{
     margin-top: 54px;
     height: calc(100vh - 114px);
+}
+.main-page {
+    color: #fff;
+    height: calc(100vh - 114px);
+    overflow-y: auto;
+}
+
+
+/*底部菜单*/
+.van-tabbar {
+    background-color: #333333;
+    border: none;
+    height: 60px;
+}
+.van-tabbar-item{
+    background-color: #333333;
 }
 </style>
